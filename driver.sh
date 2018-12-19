3fc4da407dd9ce0fa84527f207d2746153a8b6d8
trevor78 <36665098+trevor78@users.noreply.github.com>
2018-12-19 15:05:38 +0530

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -2,10 +2,10 @@
 
 . ./environment.cnf
 
-export APPLIC_NAME = whse_rcpt.exec
-export DIR_PATH = ./
+export APPLIC_NAME=whse_rcpt.exec
+export DIR_PATH=./
 
-export APP_EXEC = ${DIR_PATH}${APPLIC_NAME}
+export APP_EXEC=${DIR_PATH}${APPLIC_NAME}
 
 
 wget -c ftp://$user_name:@password@$HOST/$filename
