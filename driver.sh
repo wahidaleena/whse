7e850d7338bfdadcef0ee5b7dfddf96caa3f91d8
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-01-14 16:07:16 +0530

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -8,7 +8,7 @@ export DIR_PATH=./
 export APP_EXEC=${DIR_PATH}${APPLIC_NAME}
 
 
-wget -c ftp://$MF_USERr:@$MF_PASSWORD@MFDNT01/$filename
+wget -c ftp://$MF_USERr:@$MF_PASSWORD@MFDNT01/$FILENAME
 
 # time ${APP_EXEC}
 ./whse_rcpt.exec
